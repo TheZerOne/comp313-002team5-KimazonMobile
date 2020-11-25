af1270d9e870a98ee4d7d0c2730b6689c4aa06e8
Francis Cadiente <francis.m.p.cadiente@gmail.com>
2020-11-24 21:29:33 -0500

    Update ContactSellerForm.js

--- app/components/ContactSellerForm.js
+++ app/components/ContactSellerForm.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Alert, Keyboard } from "react-native";
 import { Notifications } from "expo";
 import * as Yup from "yup";
 
-import { Form, FormField, SubmitButton } from "./forms";
+import { Form, FormField,PickerForm, SubmitButton } from "./forms";
 import messagesApi from "../api/messages";
 
+
 function ContactSellerForm({ listing }) {
+ 
   const handleSubmit = async ({ message }, { resetForm }) => {
     Keyboard.dismiss();
-
+   
     const result = await messagesApi.send(message, listing.id);
 
     if (!result.ok) {
@@ -24,21 +26,17 @@ function ContactSellerForm({ listing }) {
       body: "Your message was sent to the seller.",
     });
   };
-
+ 
+ 
   return (
     <Form
       initialValues={{ message: "" }}
       onSubmit={handleSubmit}
       validationSchema={validationSchema}
     >
-      <FormField
-        maxLength={255}
-        multiline
-        name="message"
-        numberOfLines={3}
-        placeholder="Message..."
-      />
-      <SubmitButton title="Contact Seller" />
+
+  
+      <SubmitButton title="Add To Cart" />
     </Form>
   );
 }
